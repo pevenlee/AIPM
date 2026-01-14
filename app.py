3562ffe10be1ec3e092fd563b2a07a089bc093d1
Peiwen Li <44797525+pevenlee@users.noreply.github.com>
2026-01-14 14:14:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -501,7 +501,7 @@ st.markdown(f"""
 <div class="fixed-header-container">
     <div class="nav-left">
         <div class="nav-logo-icon">{logo_html}</div>
-        <div class="nav-logo-text">| ChatBI</div>
+        <div class="nav-logo-text">|ChatBI</div>
     </div>
     <div class="nav-right">
         <div class="nav-tag">Peiwen</div>
@@ -565,7 +565,7 @@ for msg in st.session_state.messages:
 
 # --- 猜你想问 (左对齐按钮) ---
 if not st.session_state.messages:
-    st.markdown("### 通过与医药魔方交流，开启对医药市场的探索吧！")
+    st.markdown("### Pharmcube is revolutionizing the pharmaceutical industry with data & AI.")
     c1, c2, c3 = st.columns(3)
     def handle_preset(question):
         st.session_state.messages.append({"role": "user", "type": "text", "content": question})
